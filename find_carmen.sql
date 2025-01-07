d8fc05c48f396e8c6acbac4d064c6148faf018ed
Abdulla Durrani <Abdullahkd@gmail.com>
2025-01-07 10:19:56 -0500

    CARMEN SANDIEGO - Halfway there! Hot on her trail.

--- find_carmen.sql
+++ find_carmen.sql
@@ -24,22 +24,46 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
+Commands:
+
+1. carmen=# SELECT * FROM country;
+2. carmen=# SELECT * FROM country WHERE region Southern Europe;
+
+Answer:
+VAT  | Holy See (Vatican City State) | Europe    | Southern Europe | 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
+Commands:
+carmen=# SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
 
+Answer:
+Italian
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+Commands:
 
+carmen=# SELECT * FROM countrylanguage WHERE language  = 'Italian' ORDER BY countrycode ASC;
+carmen=# SELECT * FROM country WHERE region = 'Southern Europe' ORDER BY code ASC;
 
+Answer: 
+Italy
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
+Chris Lee helped with this one!
 
+Commands:
+carmen=# SELECT * FROM country JOIN countrylanguage ON country.code = countrylanguage.countrycode
+carmen-# WHERE country.region = 'Southern Europe' AND countrylanguage.language = 'Italian';
+carmen=# SELECT * FROM countrylanguage;
+carmen=# SELECT * FROM countrylanguage ORDER BY countrycode;
+carmen=# SELECT * FROM country JOIN countrylanguage ON country.code = countrylanguage.countrycode
+WHERE country.region = 'Southern Europe' AND countrylanguage.language = 'Italian';
+carmen=# SELECT * FROM countrylanguage WHERE countrycode IN ('ITA', 'SMR', 'VAT');
 
-
-
-
+Answer: 
+San Marino
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
