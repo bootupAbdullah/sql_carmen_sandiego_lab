1672385db46b0c40c8b7459d29444a2ddeec8bd4
Abdulla Durrani <Abdullahkd@gmail.com>
2025-01-07 12:11:09 -0500

    working on clue 5

--- find_carmen.sql
+++ find_carmen.sql
@@ -67,6 +67,13 @@ San Marino
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+Commands:
+carmen=# SELECT * FROM country JOIN city ON country.code = city.countrycode WHERE country.region = 'South America' ORDER BY code ASC;
+carmen=# SELECT country.name, city.name FROM country JOIN city ON country.code = city.countrycode WHERE country.region = 'South America' ORDER BY code ASC;
+carmen=# SELECT country.name, city.name FROM country JOIN city ON country.code = city.countrycode WHERE country.region = 'South America'AND city.name LIKE '%San%' ORDER BY code ASC;
+
+carmen=# SELECT country.name, city.name FROM country JOIN city ON country.code = city.countrycode WHERE country.region = 'South America'AND city.name LIKE '%San %' ORDER BY code ASC;
+
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
